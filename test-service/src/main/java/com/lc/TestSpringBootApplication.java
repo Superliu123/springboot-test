7309869fca416eef9e074a0aaa0156092b3356a2
liu_chao <1974552862@qq.com>
2019-12-23 17:11:20 +0800

    commit

--- test-service/src/main/java/com/lc/TestSpringBootApplication.java
+++ test-service/src/main/java/com/lc/TestSpringBootApplication.java
@@ -1,12 +1,3 @@
-/**
- * All rights Reserved, Designed By Suixingpay.
- *
- * @author liu_chao[liu_chao2@suixingpay.com]
- * @date 2019-11-19 20:29
- * @version 01
- * @Copyright ©2019 Suixingpay. All rights reserved.
- * 注意：本内容仅限于随行付支付有限公司内部传阅，禁止外泄以及用于其他的商业用途。
- */
 package com.lc;
 
 import lombok.extern.slf4j.Slf4j;
--- test-service/src/main/java/com/lc/service/impl/TestServiceImpl.java
+++ test-service/src/main/java/com/lc/service/impl/TestServiceImpl.java
@@ -1,12 +1,3 @@
-/**
- * All rights Reserved, Designed By Suixingpay.
- *
- * @author liu_chao[liu_chao2@suixingpay.com]
- * @date 2019-11-20 11:49
- * @version 01
- * @Copyright ©2019 Suixingpay. All rights reserved.
- * 注意：本内容仅限于随行付支付有限公司内部传阅，禁止外泄以及用于其他的商业用途。
- */
 package com.lc.service.impl;
 
 import com.lc.domain.User;
